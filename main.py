4822f2281121176a6888319b20f16cbcf0315bfe
IGOR777 <engine777.15@gmail.com>
2024-02-17 17:12:35 +0200

    add files )

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-#Вывод четырехзначного числа в столбик
-number = 1234
-print(number // 10000)
-print((number // 1000) % 10)
-print((number // 10) % 10)
-print(number % 10)
+number = int(input("Введіть п'ятизначне число: "))
+reversed_number = (number % 10) * 10000 + ((number // 10) % 10) * 1000 + ((number // 100) % 10) * 100 + ((number // 1000) % 10) * 10 + (number // 10000)
+print(reversed_number)
