15d0f31cd1ff7d2a51f5f489ca7c356d8995c455
IGOR777 <engine777.15@gmail.com>
2024-02-24 09:10:48 +0200

    add)

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-number = 3847
-print(number // 10)
-print(number % 10)
-
-firs_number = number % 10
-number = number // 10
-second_number = number % 10
-print(second_number, firs_number)
 input_list = [1, 2, 3, 4, 5, 6]
 
 result = [input_list[:len(input_list)//2 + len(input_list) % 2], input_list[len(input_list)//2 + len(input_list) % 2:]]
