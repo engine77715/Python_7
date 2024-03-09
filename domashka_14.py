c4f14fd0c8ed034f92b020392f45a72a03a1b2a0
IGOR777 <engine777.15@gmail.com>
2024-03-09 13:50:20 +0200

    add)

--- domashka_14.py
+++ domashka_14.py
@@ -1,12 +1,28 @@
-num = int(input("Введіть ціле число: "))
+def format_time(seconds):
+    # Знайти кількість днів, годин, хвилин та секунд
+    days, seconds = divmod(seconds, 24 * 60 * 60)
+    hours, seconds = divmod(seconds, 60 * 60)
+    minutes, seconds = divmod(seconds, 60)
 
-while num >= 10:
-    product = 1
-    while num > 0:
-        digit = num % 10
-        if digit != 0:
-            product *= digit
-        num //= 10
-    num = product
+    # Підбір слова "день" в залежності від кількості днів
+    day_word = "днів" if days != 1 else "день"
 
-print(num)
+    # Форматування результату з додаванням провідних нулів
+    result = "{:02} днів {:02}:{:02}:{:02}".format(int(days), int(hours), int(minutes), int(seconds))
+    return result
+
+
+# Зчитування введеного користувачем числа секунд
+while True:
+    try:
+        user_input = int(input("Введіть кількість секунд (більше або дорівнює 0 і менше ніж 8640000): "))
+        if 0 <= user_input < 8640000:
+            break
+        else:
+            print("Будь ласка, введіть число в межах вказаного діапазону.")
+    except ValueError:
+        print("Будь ласка, введіть ціле число.")
+
+# Форматування та виведення результату
+formatted_time = format_time(user_input)
+print("Час у читабельному форматі:", formatted_time)
